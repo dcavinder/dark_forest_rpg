e95d416352ae7221abdf4714d2f43c09da245bb7
Danielle Cavinder <daniellecavinder@Danielles-MBP.home>
2022-11-21 19:13:25 -0700

    adding a user model

---
+++ models/User.js
@@ -0,0 +1,3 @@
+// For this model we should be able to ref #23>models>User.js in the 14-MVC class section
+// I think it would be a good idea to have a user id (number), user name, email, and password.
+//additionally we might condsider allowing users to add their gamer tag as well
